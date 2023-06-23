183f20a0b6b15d52a9a0f3566edf0508ae718dc0
MandarBedi <mandarbedi2002@gmail.com>
2023-06-23 08:45:35 +0530

    Updating music file 23/6/23

--- Music Player in Python/mymusicPlayer.py
+++ Music Player in Python/mymusicPlayer.py
@@ -1,24 +1,33 @@
+import os
 import pygame
-
-def MusicStoppingFunction():
-    pygame.mixer.music.stop()
 
 def MusicPlayingFunction(file_path):
     pygame.mixer.init()
     pygame.mixer.music.load(file_path)
     pygame.mixer.music.play()
-    
 
-MusicList=['Aathma Raama 2023','Maan Meri Jaan']
-
-for i in range(len(MusicList)):
-    song=MusicList[i]
-    print(str(song)+' is playing.')
-    file_path ="C:/Users/VCM/Desktop/code clause Internship project/Music Player in Python/"+str(song)+".mp3"
-    MusicPlayingFunction(file_path) 
-    # if(input("For skip this song, type 'y'else 'n': ")=='y'):
-    #     MusicStoppingFunction()
-    while pygame.mixer.music.get_busy():
-        continue
-    MusicStoppingFunction()
-print('Your Music List is over.')
+MusicList=os.listdir()
+for Song in MusicList:
+    if Song.endswith('.mp3')==True:
+        print(Song+' is playing.')
+        MusicPlayingFunction(Song)
+        while pygame.mixer.music.get_busy():
+            print("\nIt is infinity input loop so it will ask you untill the list stops.")
+            print("When the song stops playing, type 'c' to play next song.")
+            a=input("Type 'y' for skip this song, Type 'q' to quit the list, Type 'c' to continue with this song, Type 'p' to pause song: ")
+            if a=='y':
+                print(Song+'.mp3 is skipped.\n')
+                break
+            while a=='p':
+                pygame.mixer.music.pause()
+                print('The song is paused.')
+                if input("Type 'r' to resume song: ")=='r':    
+                    pygame.mixer.music.unpause()
+                    break
+                else: print('Invalid value.') 
+            if a=='q':
+                print('The list is over.')
+                exit()    
+            else: 
+                print('Plz type the correct value')
+print('The list is over.')
